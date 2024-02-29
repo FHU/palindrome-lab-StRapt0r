44c45e877936b3a8c7854ca34d61705c8d41571d
Elijah <87155048+StRapt0r@users.noreply.github.com>
2024-02-29 09:44:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def palindrome(word):
-    pass
+def palindrome(word1):
+    if word1 == "":
+        return(False)
+    elif (word1.isspace()) == False:
+        word1.replace(" ","")
+    word1 = word1.lower
+    word2 = word1[::-1]
+    if word2==word1:
+        return(True)
+    else:
+        return(False)
+
+
 
 if __name__ == '__main__': 
     #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    word=input()
+    print(palindrome(word))
+
